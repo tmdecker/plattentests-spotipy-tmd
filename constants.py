4d146e6109d8eb99db54e2bd3a6a60dbf862ebe2
Stefan Pernpaintner <stefan.pernpaintner@gmail.com>
2018-11-03 07:59:41 +0100

    externalized constants

---
+++ constants.py
@@ -0,0 +1,7 @@
+username = 'xxxx'
+scope = 'user-follow-modify playlist-modify'
+client_id = 'xxx'  
+client_secret = 'xx'  
+redirect_uri = 'https://www.plattentests.de'
+playlist_id = 'xxx'
+plattentests_api_endpoint = "https://www.plattentests.de/api/v1/reviews/latest"
